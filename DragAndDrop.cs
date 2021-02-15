a215301407ac9b0c2b2ac32151367343cc107fcc
Michael Rees <michaelrees@outlook.com>
2021-02-15 21:59:48 +0000

    Added DragAndDrop example

---
+++ DragAndDrop.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace HerokuappExamples
+{
+    [TestFixture]
+    public class DragAndDrop : BaseTest
+    {
+        internal DragAndDropPO DragAndDropPO { get; private set; } //Property for creating the DragAndDropPO in the setup
+
+        [SetUp]
+        public void ActionsRunBeforeEachTest()
+        {
+            DragAndDropPO = new DragAndDropPO(Driver);
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/drag_and_drop");
+        }
+
+        [Test]
+        public void DragAOnToB()
+        {
+            DragAndDropPO.DragAOverB();
+
+           
+        }
+    }
+}
