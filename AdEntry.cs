523e52b92de6b81fe040fe5cd98d57662ffa6ef0
Michael Rees <michaelrees@outlook.com>
2021-03-10 19:46:11 +0000

    Ad Entry modal dialog example

---
+++ AdEntry.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace HerokuappExamples
+{
+    [Category("Regression Test"), Category("Ad Entry Modal Example")]
+    [Parallelizable]
+    [TestFixture]
+    public class AdEntry : BaseTest
+    {
+        internal AdEntryPO AdEntryPO { get; private set; }
+
+        [Author("Michael Rees")]
+        [Description("Accept a Ad Entry modal dialog.")]
+        [Test]
+        public void LaunchPageAndAcceptModal()
+        {
+            AdEntryPO.LaunchPageAndAcceptModal();
+        }
+        [SetUp]
+        public void RunsBeforeEachTest()
+        {
+            AdEntryPO = new AdEntryPO(Driver);
+        }
+    }
+}
---
+++ AdEntryPO.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace HerokuappExamples
+{
+    internal class AdEntryPO : BaseApplicationPage
+    {
+        public AdEntryPO(IWebDriver driver) : base(driver) { }
+
+        /********************
+         * Page Objects Below
+         ********************/
+
+        public IWebElement EntryAdLinkText => Driver.FindElement(By.LinkText("Entry Ad"));
+        public IWebElement ModalDialog => Driver.FindElement(By.Id("modal"));
+        public IWebElement ModalClose => Driver.FindElement(By.XPath("//div[@class='modal-footer']/p"));
+
+        /***************
+         * Methods Below
+         * *************/
+        internal void LaunchPageAndAcceptModal()
+        {
+            EntryAdLinkText.Click();
+            ModalDialog.Click();
+            System.Threading.Thread.Sleep(2000);
+            ModalClose.Click();
+        }
+    }
+}
